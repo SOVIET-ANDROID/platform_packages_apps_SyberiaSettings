251774a85fa0098af47185738e1b67b278b7c910
kondors1995 <normandija1945@gmail.com>
2020-05-23 20:19:06 +0000

    Revert "Syberia: FOD pressed state [2/2]"
    This reverts commit 5c5704e073b8acdb2e17de6d233a8390fbb63df6.

--- src/com/syberia/settings/fragments/LockScreenSettings.java
+++ src/com/syberia/settings/fragments/LockScreenSettings.java
@@ -38,8 +38,8 @@ import java.util.List;
 
 public class LockScreenSettings extends SettingsPreferenceFragment implements Indexable {
 
-    private static final String FOD_ICON_PICKER_CATEGORY = "fod_icon_picker";
-    private PreferenceCategory mFODIconPickerCategory;
+    private static final String FOD_ICON_PICKER_CATEGORY = "fod_icon_picker_category";
+    private Preference mFODIconPicker;
 
     @Override
     public void onCreate(Bundle bundle) {
@@ -47,10 +47,10 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements In
         addPreferencesFromResource(R.xml.lockscreen_settings);
         final PreferenceScreen prefScreen = getPreferenceScreen();
 
-        mFODIconPickerCategory = (PreferenceCategory) findPreference(FOD_ICON_PICKER_CATEGORY);
-        if (mFODIconPickerCategory != null
+        mFODIconPicker = (Preference) findPreference(FOD_ICON_PICKER_CATEGORY);
+        if (mFODIconPicker != null
                 && !getResources().getBoolean(com.android.internal.R.bool.config_needCustomFODView)) {
-            prefScreen.removePreference(mFODIconPickerCategory);
+            prefScreen.removePreference(mFODIconPicker);
         }
 
     }
